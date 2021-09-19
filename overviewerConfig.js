cc40ba90b27f477b91533277cfa934d58b0dfc6d
kl0busch <80764930+kl0busch@users.noreply.github.com>
2021-09-19 15:18:33 +0200

    u2

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1632056424",
+        "cacheTag": "1632057435",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
